15764190403fb6821c9d48485701ba18851ee70f
unknown <ashishprajapati936@gmail.com>
2019-05-01 14:10:16 +0530

    Reverse Linked List

---
+++ LinkedList/LinkedList_Add_Node_At_Specific_Position.py
@@ -0,0 +1,66 @@
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node):
+    while node:
+        print(node.data)
+
+        node = node.next
+
+
+def insertNodeAtPosition(head, data, position):
+    node=SinglyLinkedListNode(data)
+    current=head
+    for i in range(position-1):
+        current=current.next
+    currentNext=current.next
+    node.next=currentNext
+    current.next=node
+    
+    return head
+        
+
+
+
+if __name__ == '__main__':
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    data = int(input())
+
+    position = int(input())
+
+    llist_head = insertNodeAtPosition(llist.head, data, position)
+
+    print_singly_linked_list(llist_head)
+
