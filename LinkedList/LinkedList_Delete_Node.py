b5ca8c6c71b0a6d5648711dd3f9a42f6f77267e8
unknown <ashishprajapati936@gmail.com>
2019-05-01 14:11:39 +0530

    Delete a node from LinkedList

---
+++ LinkedList/LinkedList_Delete_Node.py
@@ -0,0 +1,60 @@
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node):
+    while node:
+        print(node.data)
+        node = node.next
+
+
+def deleteNode(head, position):
+    current=head
+    if position==0:
+        headnext=head.next
+        head.next=None
+        return headnext
+    for i in range(position-1):
+        current=current.next
+    currentNext=current.next.next
+    current.next=currentNext
+    return head
+
+if __name__ == '__main__':
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    position = int(input())
+
+    llist1 = deleteNode(llist.head, position)
+
+    print_singly_linked_list(llist1)
