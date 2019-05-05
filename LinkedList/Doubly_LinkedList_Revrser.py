f42ffa6fa51893d50c2abbff6de6dababd938d2e
unknown <ashishprajapati936@gmail.com>
2019-05-05 17:37:20 +0530

    Reverse a Doubly Linked List

---
+++ LinkedList/Doubly_LinkedList_Revrser.py
@@ -0,0 +1,66 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+class DoublyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+        self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = DoublyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+
+
+        self.tail = node
+
+def print_doubly_linked_list(node, sep, fptr):
+    while node:
+        print(node.data)
+
+        node = node.next
+
+
+
+def reverse(head):
+    if not head.next:
+        head.prev=None
+        return head
+    x=reverse(head.next)
+    h=head.next
+    h.next=head
+    head.prev=h
+    head.next=None
+    return x
+
+if __name__ == '__main__':
+    
+    t = int(input())
+
+    for t_itr in range(t):
+        llist_count = int(input())
+
+        llist = DoublyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_doubly_linked_list(llist1)
+
