48cc20ff3b78a5ff6c6fc507eefe83767108f1b7
unknown <ashishprajapati936@gmail.com>
2019-05-05 18:17:05 +0530

    Check Cycle in Linked List

---
+++ LinkedList/LinkedList_Check_Cycle.py
@@ -0,0 +1,77 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node):
+    while node:
+        print(node.data)
+
+        node = node.next
+
+
+def has_cycle(head):
+    if head == None:
+        return 0
+    h1=head
+    h2=head
+    while h2!=None and h2.next!=None:
+        h1=h1.next
+        h2=h2.next.next
+        if h1==h2:
+            return 1
+    return 0
+
+if __name__ == '__main__':
+    
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        index = int(input())
+
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        extra = SinglyLinkedListNode(-1);
+        temp = llist.head;
+
+        for i in range(llist_count):
+            if i == index:
+                extra = temp
+
+            if i != llist_count-1:
+                temp = temp.next
+
+        temp.next = extra
+
+        result = has_cycle(llist.head)
+
+        print(int(result))
